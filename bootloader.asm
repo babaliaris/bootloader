bccf1aae8a4a87acfa0655e1a301be8717d00f9e
babaliaris.nikos@gmail.com <babaliaris.nikos@gmail.com>
2020-08-11 01:09:22 +0300

    Init

--- bootloader.asm
+++ bootloader.asm
@@ -1,15 +1,27 @@
 [bits 16]
 [org 0x7c00]
 
-;Initialize the stack at 0x8000 above the bootloader memory.
-;this is where the free space between the bios extended area
-;and the bootloader area exists.
-mov bp, 0x8000
-mov sp, bp
+;Initialize the stack.
+mov bp, 0x7e00 ;This is the address where the 512 bootloader ends.
+add bp, 50000  ;Set the bp and sp 50kb above the bootloader.
+mov sp, bp     ; Finally we have 50kb stack.
 
-
+;Print a static string.
 mov bx, hello_world
 call print_static
+
+;Push a string into the stack and print it.
+push 'd'   ;Push the first character.
+mov si, sp ;Store the beggining of the string.
+push 'y'
+push 'n'
+push 'a'
+push 'm'
+push 'i'
+push 'c'
+push 0     ;End the string.
+call print_dynamic
+add sp, 16 ;Deallocate the string.
 
 jmp $
 
@@ -39,6 +51,27 @@ print_static:
     print_static_end:
 
         ;Retrieve all general purpose registers.
+        popa
+        ret
+
+
+;Get a null terminated character stored in the stack and print it.
+;[in] : si (the beggining of the string)
+;[out]: void
+print_dynamic:
+    
+    pusha
+
+    print_dynamic_while:
+        mov ax, [si] ;copy the current character to al.
+        mov ah, 0x0e ;bios print char
+        cmp al, 0 ;if its zero
+        je print_dynamic_end ;stop;
+        int 0x10 ;else print al to the screen.
+        sub si, 2;go to the next character.
+        jmp print_dynamic_while ;start again.       
+
+    print_dynamic_end:
         popa
         ret
 
